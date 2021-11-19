871559ef87c664da9fceb1f07fd7158d598c5534
Suhendra <amarsuhendra2@gmail.com>
2021-11-19 23:41:34 +0700

    create method for class vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -5,6 +5,36 @@ public class Vehicle {
     private String tipebbm;
     private double harga;
 
+    public Vehicle(){
 
+    }
 
+    public Vehicle(String nama, String jenis){
+        this.nama = nama;
+        this.jenis = jenis;
+    }
+
+    protected void setHarga(double harga){
+
+    }
+
+    protected void setTransmisi(String transmisi){
+
+    }
+
+    protected void setTipeBbm(String tipebbm){
+
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public String getTransmisi() {
+        return transmisi;
+    }
+
+    public String getTipebbm() {
+        return tipebbm;
+    }
 }
