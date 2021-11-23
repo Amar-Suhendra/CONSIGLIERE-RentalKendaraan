8dde8f7f3efd40d222e372b47b7de3c814760ba8
Muhammad Rudy Hidayat <muhammadrudyhidayat@gmail.com>
2021-11-22 22:25:33 -0800

    Update Customer.java
    menambahkan method setNama dan getNama

---
+++ src/Customer.java
@@ -0,0 +1,15 @@
+public class Customer {
+    private String nama;
+    private int lama_sewa;
+    private String kontak;
+
+    //Method
+    public void setNama(String Nama){
+        nama = Nama;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+}
