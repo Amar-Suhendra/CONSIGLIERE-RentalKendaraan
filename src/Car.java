78e1e927eef2ce31b4912d96fd3b8aef7bce524b
Suhendra <amarsuhendra2@gmail.com>
2021-11-25 07:34:15 +0700

    relocation

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle {
+    private double HARGA;
+    private String TRANSMISI;
+    private String TIPEBBM;
+
+    public Car(String nama, String jenis){
+        super(nama, jenis);
+    }
+}
