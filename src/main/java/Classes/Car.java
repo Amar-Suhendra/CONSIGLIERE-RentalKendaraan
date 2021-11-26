a6f33159467e0a5236f54d58e8d96f6bda275cd1
Suhendra <amarsuhendra2@gmail.com>
2021-11-27 02:03:21 +0700

    source update

--- src/main/java/Classes/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car extends Vehicle {
-    private double HARGA;
-    private String TRANSMISI;
-    private String TIPEBBM;
-
-    public Car(String nama, String jenis){
-        super(nama, jenis);
-    }
-}
