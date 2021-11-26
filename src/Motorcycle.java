7213f9fe9e588ae8189d0537e59283bfbdf9b848
Suhendra <amarsuhendra2@gmail.com>
2021-11-26 08:01:04 +0700

    Change IDE
    Using NetBeans IDE

--- src/Motorcycle.java
+++
@@ -1,9 +0,0 @@
-public class Motorcycle extends Vehicle{
-    private double HARGA;
-    private String TRANSMISI;
-    private  String TIPEBBM;
-
-    public Motorcycle(String nama, String jenis){
-        super(nama, jenis);
-    }
-}
