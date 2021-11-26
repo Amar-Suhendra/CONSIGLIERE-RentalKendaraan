634db6f7d30ed65000625b8f84d2e8a81d6dc7bb
Suhendra <amarsuhendra2@gmail.com>
2021-11-27 01:37:17 +0700

    Change IDE

---
+++ src/main/java/Classes/Motorcycle.java
@@ -0,0 +1,9 @@
+public class Motorcycle extends Vehicle{
+    private double HARGA;
+    private String TRANSMISI;
+    private  String TIPEBBM;
+
+    public Motorcycle(String nama, String jenis){
+        super(nama, jenis);
+    }
+}
