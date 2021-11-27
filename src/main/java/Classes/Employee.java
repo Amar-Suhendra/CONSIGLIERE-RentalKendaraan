a06cfceeac824b4660856466c4828edb912cc5b0
Muhammad Rudy Hidayat <muhammadrudyhidayat@gmail.com>
2021-11-27 13:13:38 -0800

    Update

---
+++ src/main/java/Classes/Employee.java
@@ -0,0 +1,16 @@
+package Classes;
+
+public class Employee {
+    
+    private String nama;
+
+    public void setNama(String nama)
+    {
+        this.nama = nama;
+    }
+
+    public String getNama()
+    {
+        return nama;
+    }
+}
