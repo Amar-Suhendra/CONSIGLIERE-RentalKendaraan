c4cf12d2623f4a9e295081d94112671c90dfdf63
AuliaMentari <putriauliamentari266@gmail.com>
2021-11-30 20:40:32 +0700

    change employee

---
+++ src/main/java/Classes/Car.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class Car extends Vehicle {
+    private double HARGA;
+    private String TRANSMISI;
+    private String TIPEBBM;
+
+    public Car(String nama, String jenis){
+        super(nama, jenis);
+    }
+}
